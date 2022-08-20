ea5b5f08766e718b6e3f5fda7978500bab86af15
ekspertos <shxoduq98@gmail.com>
2022-08-21 03:16:30 +0900

    updates

--- korean_ASR/generate_lm_transcript/prepare_text/korean_free_speech.py
+++ korean_ASR/generate_lm_transcript/prepare_text/korean_free_speech.py
@@ -10,7 +10,6 @@ from typeguard import check_argument_types
 # https://www.aihub.or.kr/aihubdata/data/view.do?dataSetSn=109
 # |_ 자유대화음성
 #     |_ 자유대화 음성(일반남녀)
-#         |_ Training
 #           |_  일반남여_[장소#1]
 #               |_ 일반남여_일반통합_xxx_[장소#1]_xxx_00001.json
 #               |_ 일반남여_일반통합_xxx_[장소#1]_xxx_00002.json
@@ -20,11 +19,8 @@ from typeguard import check_argument_types
 #               |_ 일반남여_일반통합_xxx_[장소#1]_xxx_00001.json
 #               |_ 일반남여_일반통합_xxx_[장소#2]_xxx_00002.json
 #               |_ ...
-#        |_ Validation
-#           |_ ...
-#           |_ ...
 #
-# cat 자유대화음성/자유대화 음성(일반남녀)/Training/일반남여_[장소#1]/일반남여_일반통합_xxx_[장소#1]_xxx_00001.json
+# cat 자유대화음성/자유대화 음성(일반남녀)/일반남여_[장소#1]/일반남여_일반통합_xxx_[장소#1]_xxx_00001.json
 # {
 #   "발화정보" : {
 #     "stt" : "안녕하세요. 연습입니다.",
@@ -65,15 +61,9 @@ def prepare_text(
             flist_2 = os.listdir(subdir)
             for file_2 in flist_2:
                 subdir_2 = Path(subdir, file_2)
-                flist_3 = os.listdir(subdir_2)
-                for file_3 in flist_3:
-                    subdir_3 = Path(subdir_2, file_3)
-                    flist_4 = os.listdir(subdir_3)
-                    for file_4 in flist_4:
-                        subdir_4 = Path(subdir_3, file_4)
-                        with subdir_4.open() as f_json:
-                            json_data = json.load(f_json)
-                        f.write(str(json_data[json_category][json_sub_category]) + '\n')
+                with subdir_2.open() as f_json:
+                    json_data = json.load(f_json)
+                f.write(str(json_data[json_category][json_sub_category]) + '\n')
 
 
 
